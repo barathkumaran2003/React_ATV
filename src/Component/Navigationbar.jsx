3443c00c516364ceac32f23a4231620b106b55e9
barathkumaran2003 <barathkumaran2003@gmail.com>
2025-05-31 23:32:02 +0530

    ATV

--- src/Component/Navigationbar.jsx
+++ src/Component/Navigationbar.jsx
@@ -109,7 +109,7 @@ function Navigationbar () {
                     <i class="fas fa-shopping-cart cart-in" id='cart'></i>
                     
                    {!isAuthenticated &&  (<div className='profile-img' onClick={toggleDropdown}>
-                        <img src={user.picture} alt="sd" style={{borderRadius:'50px'}} />
+                        <img src="" alt="sd" style={{borderRadius:'50px'}} />
                         <h5>{user.name}</h5>
                         <span className="dropdown-icon">{dropdownOpen ? "▲" : "▼"}</span>
 
