7114636a3d75a9aa08438cb56f6fb139233a9cef
barathkumaran2003 <barathkumaran2003@gmail.com>
2025-05-31 17:43:00 +0530

    ATV

--- src/Component/S_Navbar.jsx
+++ src/Component/S_Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Atv.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
